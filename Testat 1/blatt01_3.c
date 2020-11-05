71d31687bca517cba41877e7c8349adc9f2e62a8
Mr-Cornflake <zeplindavid@gmail.com>
2020-11-05 21:15:09 +0100

    1.2) fertig gemacht

--- Testat 1/blatt01_3.c
+++ Testat 1/blatt01_3.c
@@ -6,7 +6,7 @@ int main() {
 
   y = y * z;
   printf("1. Ausgabe: %d\n", y);
-  printf("2. Ausgabe: %d\n", y / 3 * 3 - 3 * y / 3);
+  printf("2. Ausgabe: %d\n", y / 3 * 3 - 3 * y / 3); 
   printf("\n");
 
   return 0;
