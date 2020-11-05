3249989d311941bf2fc66e51f19608cb37cc35dc
Mr-Cornflake <zeplindavid@gmail.com>
2020-11-05 20:51:13 +0100

    aufgabe 1.1 fertig gemacht

--- Testat 1/blatt01_1.c
+++ Testat 1/blatt01_1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-  int a, b;
+  int a, b; // intialisierung der variablen a&b
 
-  printf("\n\n Hallo TUTOR! \n\n");
-  a = 22;
-  b = 20;
-  printf("Die Antwort ist: \t %d \n\n", a + b);
+  printf("\n\n Hallo TUTOR! \n\n");//ausgabe in der kommandozeile mit zwei zeilenumbrücken am ende
+  a = 22; //dekleration von a
+  b = 20; //dekleration von b
+  printf("Die Antwort ist: \t %d \n\n", a + b); //ausgabe mit %d. %d steht für einen integer wert, welcher aus a+b berechnet wird
 
-  return 0;
+  return 0;//function main gibt true an das system zurück
 }
