e8893a9b6df8f3e4b7b867aa5abaceadfa1c6455
Mr-Cornflake <zeplindavid@gmail.com>
2020-11-07 12:43:43 +0100

    Testat1 fertig

--- Testat 1/blatt01_3.c
+++ Testat 1/blatt01_3.c
@@ -2,11 +2,18 @@
 #include <stdint.h>
 
 int main() {
-  int16_t y = 1024, z = 65; // auch "short" als Datentyp verwendbar
+  int16_t y = 1024, z = 65; // auch "short" als Datentyp verwendbar 
+  //short hat den datenbereich {-32768,..,32767}
 
-  y = y * z;
-  printf("1. Ausgabe: %d\n", y);
-  printf("2. Ausgabe: %d\n", y / 3 * 3 - 3 * y / 3); 
+  y = y * z; //1024 * 65 = 66560 = wraparound bei 32767, es wird danach von -32768 hoch gezählt
+  //66560 - 32767 = 33793
+  //-32767 + 33793 = 1024
+  printf("1. Ausgabe: %d\n", y); //ausgabe von y, y ist jetzt 1025
+  printf("2. Ausgabe: %d\n", y / 3 * 3 - 3 * y / 3); // 1025 / 3=341 
+  // 341*3 =1023
+  //1023 - (3*y/3) = 1023 - 3072 /3
+  // 1023 - 3072 /3= 1023 - 1024 = -1
+  //Ausgabe ist wegen des verlustes der nachkommastellen -1
   printf("\n");
 
   return 0;
