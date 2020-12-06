be8260682ba8a9ab6673ff1a556113f7d1ef280d
Mr-Cornflake <zeplindavid@gmail.com>
2020-12-07 00:16:42 +0100

    Spiel Regeln implementiert
    Fehlt die Box Logik. mit Modulo? mir fehlt der Ansatz

--- Testat 5/blatt05.c
+++ Testat 5/blatt05.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
-#include <windows.h>
+#include <stdlib.h>
+
+/*
+1.1 Datenstruktur
+Erstellen Sie ein globales, zweidimensionales und statisches Array zur Speicherung 
+des 9×9Sudoku-Spielfelds. 
+Ein leeres Feld kann z.B. durch die „0“ repräsentiert werden
+*/
+
+const int Base_Feld[9][9] = {{4, 1, 0, 0, 6, 5, 0, 0, 7},
+                             {0, 0, 6, 0, 0, 7, 4, 8, 0},
+                             {2, 0, 7, 4, 9, 0, 0, 0, 6},
+                             {0, 6, 0, 0, 7, 0, 1, 0, 0},
+                             {3, 0, 1, 5, 0, 0, 0, 7, 2},
+                             {0, 9, 0, 0, 4, 2, 3, 0, 8},
+                             {1, 0, 8, 6, 0, 0, 0, 2, 9},
+                             {0, 2, 0, 0, 1, 8, 6, 4, 0},
+                             {6, 0, 0, 3, 0, 0, 0, 1, 0}};
+
+int Feld[9][9]; //spielfeld
 
 /*
 1.2 Bildschirmausgabe
@@ -9,12 +28,13 @@ wie in oberer Abbildungübersichtlich auf dem Bildschirm ausgibt
 */
 
 void drucken(int (*ptr)[9])
-{
-    printf("Folgendes sind die 4 Sudokuregeln:\n\n");
+{  
+    printf("\n\nFolgendes sind die 4 Sudokuregeln:\n\n");
     printf("\t1.\tDie Werte m\x81ssen im Bereich von 1 bis 9 sein\n");
     printf("\t2.\tIn einer Reihe darf keine Zahl mehr als ein mal auftreten\n");
     printf("\t3.\tIn einer Spalte darf keine Zahl mehr als ein mal auftreten\n");
     printf("\t4.\tIn einem Block darf keine Zahl mehr als ein mal auftreten\n\n\n");
+
 
     printf("      1  2  3     4  5  6     7  8  9\n");
     printf("   +-----------+-----------+-----------+\n");
@@ -51,33 +71,20 @@ void drucken(int (*ptr)[9])
 int main()
 {
 
-    /*
-1.1 Datenstruktur
-Erstellen Sie ein globales, zweidimensionales und statisches Array zur Speicherung 
-des 9×9Sudoku-Spielfelds. 
-Ein leeres Feld kann z.B. durch die „0“ repräsentiert werden
-*/
-    int Base_Feld[9][9] = {{4, 1, 0, 0, 6, 5, 0, 0, 7},
-                      {0, 0, 6, 0, 0, 7, 4, 8, 0},
-                      {2, 0, 7, 4, 9, 0, 0, 0, 6},
-                      {0, 6, 0, 0, 7, 0, 1, 0, 0},
-                      {3, 0, 1, 5, 0, 0, 0, 7, 2},
-                      {0, 9, 0, 0, 4, 2, 3, 0, 8},
-                      {1, 0, 8, 6, 0, 0, 0, 2, 9},
-                      {0, 2, 0, 0, 1, 8, 6, 4, 0},
-                      {6, 0, 0, 3, 0, 0, 0, 1, 0}};
 
 
-    int Feld[9][9] = {{4, 1, 0, 0, 6, 5, 0, 0, 7},
-                      {0, 0, 6, 0, 0, 7, 4, 8, 0},
-                      {2, 0, 7, 4, 9, 0, 0, 0, 6},
-                      {0, 6, 0, 0, 7, 0, 1, 0, 0},
-                      {3, 0, 1, 5, 0, 0, 0, 7, 2},
-                      {0, 9, 0, 0, 4, 2, 3, 0, 8},
-                      {1, 0, 8, 6, 0, 0, 0, 2, 9},
-                      {0, 2, 0, 0, 1, 8, 6, 4, 0},
-                      {6, 0, 0, 3, 0, 0, 0, 1, 0}};
 
+    
+    
+    
+    // Kopiere das original SUDOKU ins Spielfeld.
+    for (int row = 0; row < 9; row++)
+    {
+        for (int col = 0; col < 9; col++)
+        {
+            Feld[row][col] = Base_Feld[row][col];
+        }
+    }
 
     /*
 1.3
@@ -99,12 +106,19 @@ Nutzen Sie dazu die scanf-Funktion oder für zeichenweises Einlesen die getchar-
     {
         for (int j = 0; j < 9; j++)
         {
-            if (Feld[i][j] == 0)
+            if (Base_Feld[i][j] == 0)
             {
                 empty_fields++;
             }
         }
     }
+
+    //..............................
+    //Main Game Loop
+    //Aufgabe 1.4 - Spiel spielbar machen
+    //..............................
+
+
 
     while (empty_fields != 0) //solange nicht jedes feld ausgefüllt ist
     {
@@ -113,56 +127,79 @@ Nutzen Sie dazu die scanf-Funktion oder für zeichenweises Einlesen die getchar-
         //unabhängig von der Fenstergröße. Vorteil gegenüber system("clear") ist
         //die viel höhere effizienz
         //Fehl Eingabe Managment
-        system("clear");
+        
         drucken(Feld);
         //Wert eingabe
-        new:
-        printf("\n\n\nF\x81lle das Sudoku aus!\n\n");//goto anweisung. bei Fehler wird hier nochmal gestartet
+        new : printf("\n\n\nF\x81lle das Sudoku aus!\n\n"); //goto anweisung. bei Fehler wird hier nochmal gestartet
         printf("\tZeile:");
-        scanf("%d", &row_select);//reihe einlesen
-        
-        if (row_select>9||row_select<1)//row ausserhalb ddes Bereiches eingegeben
+        scanf("%d", &row_select); //reihe einlesen
+
+        if (row_select > 9 || row_select < 1) //row ausserhalb ddes Bereiches eingegeben
         {
-            printf("\nFehler:\tDeine Eingabe ist%d. Die Reihe darf jedoch nur im Bereich von 1 bis 9 sein\n",row_select);
+            printf("\nFehler:\tDeine Eingabe ist%d. Die Reihe darf jedoch nur im Bereich von 1 bis 9 sein\n", row_select);
             goto new;
         }
 
         printf("\tSpalte:");
-        scanf("%d", &column_select);//spalte einlesen
+        scanf("%d", &column_select); //spalte einlesen
 
-        if (Base_Feld[row_select-1][column_select-1]!=0)//das ausgewählte Feld gehört zur vorlage und soll nicht geändert werden
+        if (Base_Feld[row_select - 1][column_select - 1] != 0) //das ausgewählte Feld gehört zur vorlage und soll nicht geändert werden
         {
             printf("\nFehler:\tDas ausgew\x84hlte Feld geh\x94rt zu den vorgegebenen Feldern und kann deswegen nicht ver\x84ndert werden.\n");
             goto new;
         }
 
-        if (column_select>9||column_select<1)//spalte ist ausserhalb des Bereiches
+        if (column_select > 9 || column_select < 1) //spalte ist ausserhalb des Bereiches
         {
-            printf("\nFehler:\tDeine Eingabe ist%d. Die Spalte darf jedoch nur im Bereich von 1 bis 9 sein\n",column_select);
+            printf("\nFehler:\tDeine Eingabe ist%d. Die Spalte darf jedoch nur im Bereich von 1 bis 9 sein\n", column_select);
             goto new;
         }
 
         printf("\tWelcher Wert soll eingegeben werden?:");
-        scanf("%d", &new_value);//Wert für das Kästchen eingeben
+        scanf("%d", &new_value); //Wert für das Kästchen eingeben
 
         //regel 1: Bereich beachten
-        if (new_value>9||new_value<1)//spalte ist ausserhalb des Bereiches
+        if (new_value > 9 || new_value < 1) //spalte ist ausserhalb des Bereiches
         {
-            printf("\nFehler:\tDeine Eingabe ist%d. Der Wert darf jedoch nur im Bereich von 1 bis 9 sein\n",new_value);
+            printf("\nFehler:\tDeine Eingabe ist %d. Der Wert darf jedoch nur im Bereich von 1 bis 9 sein\n", new_value);
             goto new;
         }
-        
 
         //------------------------------------------------------
-        //alle Gültigkeiten abgearbeitet, jetzt nur noch logik fehler möglich. 
+        //alle Gültigkeiten abgearbeitet, jetzt nur noch logik fehler möglich.
         //Verbesserung durch das üebrschreiben von Feldern.
         //------------------------------------------------------
         column_select--;
         row_select--;
-        Feld[column_select][row_select]=new_value;
+        
+        
+
+        //spalten&reihen Regel
+        Feld[row_select][column_select] = new_value;
+       for (int row = 0; row < 9; row++)
+       {
+           for (int col = 0; col < 9; col++)
+           {
+               if(Feld[row_select][column_select]==Base_Feld[row_select][col]){
+                   system("cls");
+                   printf("\n\nIn der Spalte %d und Reihe %d gibt es bereits den Eintrag %d\n\n", col+1, row_select+1, new_value);
+                   goto set;
+               }
+               if(Feld[row_select][column_select]==Base_Feld[row][column_select]){
+                   system("cls");
+                   printf("\n\nIn der Reihe %d und Spalte %d gibt es bereits den Eintrag %d\n\n", row+1, column_select+1, new_value);
+                   goto set;
+               }
+           }
+       }
+
+        //TODO: Box regel
+       
+        
+
+       set:
+    empty_fields--;
     }
-
-
 
     return 0;
 }
