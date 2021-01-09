c22f34b03f101c971ac42f8b6083f454e69252c5
heydoma <74253024+heydoma@users.noreply.github.com>
2021-01-09 16:05:11 +0100

    blatt08

---
+++ Testat 8/sort.h
@@ -0,0 +1,16 @@
+//
+//  sort.h
+//  blatt08
+//
+//  Created by Marvin Heydorn on 09.01.21.
+//
+
+#ifndef sort_h
+#define sort_h
+
+#include <stdio.h>
+
+void bubbleSort(int *array,int length);
+
+void mergeSort(int *array, int length);
+#endif /* sort_h */
